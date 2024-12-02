994b9f75463b358e19cc09eedb12897dc1519f62
zhushuihua <zhushuihua@yahoo.com>
2024-12-02 16:34:19 +0800

    second

--- src/routes/json/+page.svelte
+++ src/routes/json/+page.svelte
@@ -1,3 +1,12 @@
 <div>
 	<h1>Json Page</h1>
+	<nav>
+		<ul class="grid grid-cols-3 gap-2">
+			{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] as index}
+				<li class="bg-sky-200 h-8 flex justify-center items-center">
+					<a href="/json/{index}">{index}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
 </div>
