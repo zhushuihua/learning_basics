9dab9937b16b07eedff86f01eff0ae82bf14f122
zhushuihua <zhushuihua@yahoo.com>
2024-12-03 14:04:17 +0800

    delete

--- src/routes/form/+page.server.ts
+++ src/routes/form/+page.server.ts
@@ -1,9 +1,13 @@
 import { add, items } from './data'
 export const actions = {
-    default: async ({ request, cookies }) => {
+    create: async ({ request, cookies }) => {
         const item = await request.formData();
         add(item.get('task') as string)
     },
+    delete: async ({ request, cookies }) => {
+        const data = await request.formData()
+        items.splice(+(data.get('id') as string), 1);
+    }
 }
 export function load() {
     return {
